ca30451ea9bc8efff9af9c6d9563867a8d622c0d
EGeiss <egeiss03@gmail.com>
2018-01-24 19:09:57 -0500

    Yo Yo Yo, I didn't change anything

--- src/org/usfirst/frc/team5263/robot/commands/DriveTo.java
+++ src/org/usfirst/frc/team5263/robot/commands/DriveTo.java
@@ -43,7 +43,7 @@ public class DriveTo extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        drive(.5, .5);
+        drive(10, .5);
     }
 
     // Make this return true when this Command no longer needs to run execute()
