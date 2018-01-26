f758f6e913f6538cb282db67885aa6048a98bad1
KatieCrosthwaite <katiecrosthwaite@gmail.com>
2018-01-26 17:21:41 -0500

    Added gyro to smartdashboard

--- src/org/usfirst/frc/team5263/robot/commands/Rotation.java
+++ src/org/usfirst/frc/team5263/robot/commands/Rotation.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team5263.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
 import org.usfirst.frc.team5263.robot.Robot;
@@ -22,13 +23,21 @@ public class Rotation extends Command {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
-
+	
+	public double getGyroAngle() {
+		
+		double gyroAngle = DriveTrain.gyro.getAngle();
+		return gyroAngle * (360.0/325.0); //(360.0/330.0); //327 on comp bot, 340 on test bot
+	}
+	
     // Called just before this Command runs the first time
     protected void initialize() {
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    SmartDashboard.putNumber("gyro: ", getGyroAngle());
+    
     }
 
     // Make this return true when this Command no longer needs to run execute()
