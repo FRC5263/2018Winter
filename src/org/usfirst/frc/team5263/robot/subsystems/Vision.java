f5452be5806fd6ddb1063384d506d25bcc420f8f
Isaiah Kahler <isaiahwkahler@gmail.com>
2018-02-10 16:05:26 -0500

    added camera servos to vision subsystem

--- src/org/usfirst/frc/team5263/robot/subsystems/Vision.java
+++ src/org/usfirst/frc/team5263/robot/subsystems/Vision.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5263.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -9,10 +10,21 @@ public class Vision extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+	private static Servo camAxisX = new Servo(2);
+	private static Servo camAxisY = new Servo(3);
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
+    public static void setCamAxisX(double rotation) {
+    	camAxisX.set(rotation);
+    }
+    
+    public static void setCamAxisY(double rotation) {
+    	camAxisY.set(rotation);
+    }
+    
 }
 
