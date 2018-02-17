2008c75550742362ef3c2e71bb60527f78dc5e02
katie <katiecrosthwaite@gmail.com>
2018-02-17 10:40:38 -0500

    Made RotatePID use the shared turnController.

--- src/org/usfirst/frc/team5263/robot/commands/RotatePID.java
+++ src/org/usfirst/frc/team5263/robot/commands/RotatePID.java
@@ -2,11 +2,7 @@ package org.usfirst.frc.team5263.robot.commands;
 
 import org.usfirst.frc.team5263.robot.Robot;
 import org.usfirst.frc.team5263.robot.subsystems.DriveTrain;
-
-import com.kauailabs.navx.frc.AHRS;
-
 import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -14,41 +10,17 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class RotatePID extends Command {
 
-	PIDController turnController;
-	AHRS ahrs;
-	static final double kP = 0.03;
-	static final double kI = 0.00;
-	static final double kD = 0.00;
-	static final double kF = 0.00;
-
-	static final double kToleranceDegrees = 2.0f;
-
-	private double degrees;
-
+	PIDController turnController = DriveTrain.turnController;
+	double angle;
 	public RotatePID(double angle) {
-		
 		requires(Robot.myDrive);
-		ahrs = DriveTrain.ahrs;
-		
-		turnController = new PIDController(kP, kI, kD, kF, ahrs,  new PIDOutput() {
-			@Override
-			public void pidWrite(double output) {
-				DriveTrain.drive(output, -output);
-			}
-		});
-		
-        turnController.setInputRange(-180.0f,  180.0f);
-        turnController.setOutputRange(-1.0, 1.0);
-        turnController.setAbsoluteTolerance(kToleranceDegrees);
-        turnController.setContinuous(true);
-        
-        this.degrees = angle;
+		this.angle = angle;
 	}
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
-		turnController.setSetpoint(degrees);
-//		turnController.enable();
+		turnController.setSetpoint(angle);
+		turnController.enable();
 	}
 
 	// Called repeatedly when this Command is scheduled to run
@@ -64,12 +36,12 @@ public class RotatePID extends Command {
 	// Called once after isFinished returns true
 	protected void end() {
 		turnController.disable();
-		ahrs.reset(); //maybee
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
+		turnController.disable();
 	}
 	
 }
