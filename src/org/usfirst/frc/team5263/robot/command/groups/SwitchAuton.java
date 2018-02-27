32e1e3e14e8d0ef8638ac2dfe3fbc764883eb5ed
Evan G <egeiss03@gmail.com>
2018-02-27 15:23:25 -0500

    Auton

--- src/org/usfirst/frc/team5263/robot/command/groups/SwitchAuton.java
+++ src/org/usfirst/frc/team5263/robot/command/groups/SwitchAuton.java
@@ -12,31 +12,103 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class SwitchAuton extends CommandGroup {
 
-    public SwitchAuton() {
+	public String blueOrRed;
+	public String LRC;
+    public SwitchAuton(String blueOrRed, String LRC) {
     	String gameData;
     	gameData = DriverStation.getInstance().getGameSpecificMessage();
     	
-    	//This function runs if the field data is for the left side
-    	if(gameData.length() > 0) {
-    		if(gameData.charAt(0) == 'L'){
-    			System.out.println("Run Left Auto");
-    			
-    			addSequential(new DriveTo(12, .4, 0));
-    	    	addSequential(new RotatePID(90));
-    	    	
-    	    	addSequential(new DriveTo(1, .4, 90));
-    	    	addSequential(new FlipBucket());
-    			//This function runs if the data is for the right side
-    		}else {
-    			System.out.println("Run Right Auto");
+    	this.blueOrRed = blueOrRed;
+    	this.LRC = LRC;
+    	
+    	if(LRC == "L"){
     		
-    			addSequential(new DriveTo(16.5, .4, 0));
-    	    	addSequential(new RotatePID(90));
-    	    	addSequential(new DriveTo(12.75, .4, 90));
-    	    	addSequential(new RotatePID(180));
-    	    	addSequential(new FlipBucket());
-    			
-    		}
-    	}	
+    		if(gameData.length() > 0) {
+        		if(gameData.charAt(0) == 'L'){
+        			System.out.println("Run Left Auto");
+        			
+        			addSequential(new DriveTo(12, .4, 0));
+        	    	addSequential(new RotatePID(90));
+        	    	
+        	    	addSequential(new DriveTo(1, .4, 90));
+        	    	addSequential(new FlipBucket());
+        			//This function runs if the data is for the right side
+        		}else {
+        			System.out.println("Run Right Auto");
+        		
+        			addSequential(new DriveTo(16.5, .4, 0));
+        	    	addSequential(new RotatePID(90));
+        	    	
+        	    	addSequential(new DriveTo(12.75, .4, 90));
+        	    	addSequential(new RotatePID(180));
+        	    	
+        	    	addSequential(new FlipBucket());
+        			
+        		}
+        	}
+    		
+		}else if(LRC == "R"){
+			if(gameData.length() > 0) {
+	    		if(gameData.charAt(0) == 'L'){
+	    			System.out.println("Run Left Auto");
+	    			
+	    			addSequential(new DriveTo(12, .4, 0));
+	    	    	addSequential(new RotatePID(90));
+	    	    	
+	    	    	addSequential(new DriveTo(1, .4, 90));
+	    	    	addSequential(new FlipBucket());
+	    			//This function runs if the data is for the right side
+	    		}else {
+	    			System.out.println("Run Right Auto");
+	    		
+	    			addSequential(new DriveTo(16.5, .4, 0));
+	    	    	addSequential(new RotatePID(90));
+	    	    	addSequential(new DriveTo(12.75, .4, 90));
+	    	    	addSequential(new RotatePID(180));
+	    	    	addSequential(new FlipBucket());
+	    			
+	    		}
+	    	}
+		}else if(LRC == "C"){
+			if(gameData.length() > 0) {
+	    		if(gameData.charAt(0) == 'L'){
+	    			System.out.println("Run Left Auto");
+	    			
+	    			addSequential(new DriveTo(5, .4, 0));
+	    	    	addSequential(new RotatePID(-90));
+	    	    	
+	    	    	addSequential(new DriveTo(2, .4, -90));
+	    	    	addSequential(new RotatePID(0));
+	    	    	
+	    	    	addSequential(new DriveTo(2, .4, 0));
+	    	    	
+	    	    	addSequential(new FlipBucket());
+	    			//This function runs if the data is for the right side
+	    		}else {
+	    			System.out.println("Run Right Auto");
+	    		
+	    			addSequential(new DriveTo(5, .4, 0));
+	    	    	addSequential(new RotatePID(90));
+	    	    	
+	    	    	addSequential(new DriveTo(2, .4, 90));
+	    	    	addSequential(new RotatePID(0));
+	    	    	
+	    	    	addSequential(new DriveTo(2, .4, 0));
+	    			
+	    	    	addSequential(new FlipBucket());
+	    			
+	    		}
+	    	}
+		}else{
+			System.out.println("No");
+		}	
+    	
+    	//This function runs if the field data is for the left side
+    		
     }
+    
+    private static void goLRC(){
+		
+	}
+    
 }
