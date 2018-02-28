16b23b14bdfd07c31a1c2e55019948c5918152ce
Isaiah Kahler <isaiahwkahler@gmail.com>
2018-02-28 18:59:01 -0500

    moved auto logic from contstructor to initialize

--- src/org/usfirst/frc/team5263/robot/command/groups/SwitchAuton.java
+++ src/org/usfirst/frc/team5263/robot/command/groups/SwitchAuton.java
@@ -12,100 +12,108 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class SwitchAuton extends CommandGroup {
 
-	public String blueOrRed;
-	public String LRC;
+	private String blueOrRed;
+	private String LRC;
+	public String gameData;
+	
     public SwitchAuton(String blueOrRed, String LRC) {
-    	String gameData;
-    	gameData = DriverStation.getInstance().getGameSpecificMessage();
     	
     	this.blueOrRed = blueOrRed;
     	this.LRC = LRC;
     	
+    }
+    
+    protected void initialize() {
+    	this.gameData = DriverStation.getInstance().getGameSpecificMessage();
     	if(LRC == "L"){
     		
     		if(gameData.length() > 0) {
-        		if(gameData.charAt(0) == 'L'){
-        			System.out.println("Run Left Auto");
-        			
-        			addSequential(new DriveTo(12, .4, 0));
-        	    	addSequential(new RotatePID(90));
-        	    	
-        	    	addSequential(new DriveTo(1, .4, 90));
-        	    	addSequential(new FlipBucket());
-        			//This function runs if the data is for the right side
-        		}else {
-        			System.out.println("Run Right Auto");
-        		
-        			addSequential(new DriveTo(16.5, .4, 0));
-        	    	addSequential(new RotatePID(90));
-        	    	
-        	    	addSequential(new DriveTo(12.75, .4, 90));
-        	    	addSequential(new RotatePID(180));
-        	    	
-        	    	addSequential(new FlipBucket());
-        			
-        		}
-        	}
+    			if(gameData.charAt(0) == 'L'){
+    				System.out.println("Run Left Auto");
+    				
+    				addSequential(new DriveTo(12, .4, 0));
+    				addSequential(new RotatePID(90));
+    				
+    				addSequential(new DriveTo(1, .4, 90));
+    				addSequential(new FlipBucket());
+    				//This function runs if the data is for the right side
+    			}else {
+    				System.out.println("Run Right Auto");
+    				
+    				addSequential(new DriveTo(16.5, .4, 0));
+    				addSequential(new RotatePID(90));
+    				
+    				addSequential(new DriveTo(12.75, .4, 90));
+    				addSequential(new RotatePID(180));
+    				
+    				addSequential(new FlipBucket());
+    				
+    			}
+    		}
     		
-		}else if(LRC == "R"){
-			if(gameData.length() > 0) {
-	    		if(gameData.charAt(0) == 'L'){
-	    			System.out.println("Run Left Auto");
-	    			
-	    			addSequential(new DriveTo(12, .4, 0));
-	    	    	addSequential(new RotatePID(90));
-	    	    	
-	    	    	addSequential(new DriveTo(1, .4, 90));
-	    	    	addSequential(new FlipBucket());
-	    			//This function runs if the data is for the right side
-	    		}else {
-	    			System.out.println("Run Right Auto");
-	    		
-	    			addSequential(new DriveTo(16.5, .4, 0));
-	    	    	addSequential(new RotatePID(90));
-	    	    	addSequential(new DriveTo(12.75, .4, 90));
-	    	    	addSequential(new RotatePID(180));
-	    	    	addSequential(new FlipBucket());
-	    			
-	    		}
-	    	}
-		}else if(LRC == "C"){
-			if(gameData.length() > 0) {
-	    		if(gameData.charAt(0) == 'L'){
-	    			System.out.println("Run Left Auto");
-	    			
-	    			addSequential(new DriveTo(5, .4, 0));
-	    	    	addSequential(new RotatePID(-90));
-	    	    	
-	    	    	addSequential(new DriveTo(2, .4, -90));
-	    	    	addSequential(new RotatePID(0));
-	    	    	
-	    	    	addSequential(new DriveTo(2, .4, 0));
-	    	    	
-	    	    	addSequential(new FlipBucket());
-	    			//This function runs if the data is for the right side
-	    		}else {
-	    			System.out.println("Run Right Auto");
-	    		
-	    			addSequential(new DriveTo(5, .4, 0));
-	    	    	addSequential(new RotatePID(90));
-	    	    	
-	    	    	addSequential(new DriveTo(2, .4, 90));
-	    	    	addSequential(new RotatePID(0));
-	    	    	
-	    	    	addSequential(new DriveTo(2, .4, 0));
-	    			
-	    	    	addSequential(new FlipBucket());
-	    			
-	    		}
-	    	}
-		}else{
-			System.out.println("No");
-		}	
+    	}else if(LRC == "R"){
+    		if(gameData.length() > 0) {
+    			if(gameData.charAt(0) == 'L'){
+    				System.out.println("Run Left Auto");
+    				
+    				addSequential(new DriveTo(12, .4, 0));
+    				addSequential(new RotatePID(90));
+    				
+    				addSequential(new DriveTo(1, .4, 90));
+    				addSequential(new FlipBucket());
+    				//This function runs if the data is for the right side
+    			}else {
+    				System.out.println("Run Right Auto");
+    				
+    				addSequential(new DriveTo(16.5, .4, 0));
+    				addSequential(new RotatePID(90));
+    				addSequential(new DriveTo(12.75, .4, 90));
+    				addSequential(new RotatePID(180));
+    				addSequential(new FlipBucket());
+    				
+    			}
+    		}
+    	}else if(LRC == "C"){
+    		if(gameData.length() > 0) {
+    			if(gameData.charAt(0) == 'L'){
+    				System.out.println("Run Left Auto");
+    				
+    				addSequential(new DriveTo(5, .4, 0));
+    				addSequential(new RotatePID(-90));
+    				
+    				addSequential(new DriveTo(2, .4, -90));
+    				addSequential(new RotatePID(0));
+    				
+    				addSequential(new DriveTo(2, .4, 0));
+    				
+    				addSequential(new FlipBucket());
+    				//This function runs if the data is for the right side
+    			}else {
+    				System.out.println("Run Right Auto");
+    				
+    				addSequential(new DriveTo(5, .4, 0));
+    				addSequential(new RotatePID(90));
+    				
+    				addSequential(new DriveTo(2, .4, 90));
+    				addSequential(new RotatePID(0));
+    				
+    				addSequential(new DriveTo(2, .4, 0));
+    				
+    				addSequential(new FlipBucket());
+    				
+    			}
+    		}
+    	}else{
+    		System.out.println("No");
+    	}	
     	
     	//This function runs if the field data is for the left side
-    		
+    	
+    	
+    	
+    	
     }
+    
     
     private static void goLRC(){
 		
